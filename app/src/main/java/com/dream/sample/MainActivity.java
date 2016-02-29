72f873e3ed3621c9d99c252f7f75fb11473c2aa3
粟松 <susong@susongdeMacBook-Pro.local>
2016-02-29 19:08:40 +0800

    添加PhotoSelector

--- app/src/main/java/com/dream/sample/MainActivity.java
+++ app/src/main/java/com/dream/sample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dream.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dream.photoselector.ui.PhotoSelectorActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, PhotoSelectorActivity.class);
+                intent.putExtra(PhotoSelectorActivity.KEY_MAX, 9);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivityForResult(intent, 1);
+            }
+        }, 3000);
     }
 }
